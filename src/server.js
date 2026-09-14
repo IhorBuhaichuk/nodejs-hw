55b2f4628464f71f29eb0415e3d36b84c0144727
Ihor Buhaichuk <ihorbugaichuk@gmail.com>
2026-09-15 00:01:47 +0300

    Bind server for Render deployment

--- src/server.js
+++ src/server.js
@@ -43,6 +43,6 @@ app.use((error, _request, response, _next) => {
 
 const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
